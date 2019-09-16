99c3acfc3a1b862956b8c5e58ae4ea08474d8cdf
redricasa <fredeb26@gmail.com>
2019-09-15 22:18:58 -0700

    added add-text.js file for grabbing text input and displaying it in the BOX area

---
+++ assets/javascript/add-text.js
@@ -0,0 +1,7 @@
+//add the text from the Add Quote! form input to the BOX area where both the gif and text are displayed
+var text = $("#addText")
+$("#add-text-button").on("click", function(event){
+    event.preventDefault();
+    $("#textContainer").text(text);
+    console.log(text);
+});
