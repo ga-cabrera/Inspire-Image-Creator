da4e91a0631a0062365454216a4880953f9523b6
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2019-09-17 13:41:37 -0700

    clone quote text

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -34,5 +34,5 @@ $("#random-quote").on("click", function () {
 //Add Quote to image
 $("#add-quote").on("click", function(){
     $("#textContainer").empty();
-    $("#textContainer").append(quote, author);
+    $("#quote-bin").clone().appendTo("#textContainer");
 });
