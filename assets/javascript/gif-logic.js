26d2aab159fee4329cfa365771d587eb91d44594
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2019-09-18 02:47:24 -0700

    img size 600x400

--- assets/javascript/gif-logic.js
+++ assets/javascript/gif-logic.js
@@ -1,7 +1,7 @@
 //function to query random images from picsum
 $("#image-button").on("click", function(){
   var number = Math.floor(Math.random()*1085);
-  var queryURL = "https://picsum.photos/id/" + number + "/400/400"
+  var queryURL = "https://picsum.photos/id/" + number + "/600/400"
   var img = $("<img>");
   img.attr("src", queryURL)
   img.attr("id", "picsum-image")
