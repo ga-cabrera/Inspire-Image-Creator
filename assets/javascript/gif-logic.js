389a3b59bcb0da9751dbecab1574f2a492b30e03
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2019-09-18 10:52:20 -0700

    img and canvas populate in same div

--- assets/javascript/gif-logic.js
+++ assets/javascript/gif-logic.js
@@ -1,11 +1,9 @@
 //function to query random images from picsum
 $("#image-button").on("click", function(){
-  var number = Math.floor(Math.random()*1085);
-  var queryURL = "https://picsum.photos/id/" + number + "/600/400"
+  var queryURL = "https://picsum.photos/600/400"
   var img = $("<img>");
   img.attr("src", queryURL)
   img.attr("id", "picsum-image")
-  console.log(img);
   $("#image-container").empty();
   $(img).appendTo("#image-container");
 });
--- assets/javascript/gif-text-comb.js
+++ assets/javascript/gif-text-comb.js
@@ -2,14 +2,17 @@ function myCanvas() { // what does this function do? It appears it's just settin
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     var img = document.getElementById("picsum-image"); // where does "gif-image" exist?
-    var quote = document.getElementById("textContainer").textContent;//Random quote API area
     ctx.drawImage(img,10,10);
-    ctx.font = "25px Georgia";
-    var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
-    gradient.addColorStop("0", "magenta");
-    gradient.addColorStop("0.5", "blue");
-    gradient.addColorStop("1.0", "red");
-    ctx.fillStyle = gradient;
-    ctx.fillText(quote, 150, 50,399);
+    $("#image-container").empty();
+    $(img).appendTo("#image-container");
+
+    // var quote = document.getElementById("textContainer").textContent;//Random quote API area
+    // ctx.font = "25px Georgia";
+    // var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
+    // gradient.addColorStop("0", "magenta");
+    // gradient.addColorStop("0.5", "blue");
+    // gradient.addColorStop("1.0", "red");
+    // ctx.fillStyle = gradient;
+    // ctx.fillText(quote, 150, 50,399);
 
 }
