48c28ab2338887f7cabbe05473c8154c87970d7b
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-09-18 19:09:53 -0700

    fixed links to js

--- assets/javascript/picsum-logic.js
+++ assets/javascript/picsum-logic.js
@@ -2,12 +2,11 @@
 $("#image-button").on("click", function(){
   var number = Math.floor(Math.random()*1085);
   $("#image-container").empty();
-  var queryURL = "https://picsum.photos/id/" + number + "/600/400";
+  var queryURL = `https://picsum.photos/id/${number}/600/400`
   var img = $("<img>");
   img.attr("src", queryURL)
   img.attr("id", "picsum-image")
   $(img).appendTo("#image-container");
   console.log(number);
-  addTextToCanvas();
 });
     
